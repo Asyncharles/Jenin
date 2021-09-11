ac60f1891e7e815485bdc1c8d6b99e146313f040
FinalVoid <finalvoiddev@gmail.com>
2021-09-11 00:04:16 -0400

    Added consumer for jedis resource pool

--- src/main/java/net/charles/Jenin.java
+++ src/main/java/net/charles/Jenin.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Jenin extends JeninParser {
@@ -43,48 +44,46 @@ public class Jenin extends JeninParser {
 
     @Override
     public void push(String key, String json) {
-        jedisResource(jedis -> jedis.set(key, json));
+        withJedis(jedis -> jedis.set(key, json));
     }
 
     @Override
     public <T> void push(T t) {
-        jedisResource(jedis -> {
+        withJedis(jedis -> {
             try {
                 String key = KeyManager.findKey(t);
                 jedis.set(key, getGson().toJson(t, t.getClass()));
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-            return null;
         });
     }
 
     @Override
     public <T> void pushToHashSet(T t) {
-        jedisResource(jedis -> {
+        withJedis(jedis -> {
             try {
                 String key = KeyManager.findKey(t);
                 jedis.hset(key, convertToHashSet(t));
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-            return null;
         });
     }
 
     @Override
     public void pushToHashSet(String key, Map<String, String> obj) {
-        jedisResource(jedis -> jedis.hset(key, obj));
+        withJedis(jedis -> jedis.hset(key, obj));
     }
 
     @Override
     public <T> T search(String key, Class<T> clazz) {
-        return jedisResource(jedis -> getGson().fromJson(jedis.get(key), clazz));
+        return getWithJedis(jedis -> getGson().fromJson(jedis.get(key), clazz));
     }
 
     @Override
     public String search(String key, String fieldName, Class<?> clazz) {
-        return jedisResource(jedis -> {
+        return getWithJedis(jedis -> {
             Object obj = getGson().fromJson(jedis.get(key), clazz);
             return getGson().toJsonTree(obj).getAsJsonObject().get(fieldName).getAsString();
         });
@@ -92,17 +91,17 @@ public class Jenin extends JeninParser {
 
     @Override
     public <T> T hashSearch(String key, Class<T> clazz) {
-        return jedisResource(jedis -> convertToObject(key, jedis.hgetAll(key), clazz));
+        return getWithJedis(jedis -> convertToObject(key, jedis.hgetAll(key), clazz));
     }
 
     @Override
     public String hashSearch(String key, String fieldName) {
-        return jedisResource(jedis -> jedis.hget(key, fieldName));
+        return getWithJedis(jedis -> jedis.hget(key, fieldName));
     }
 
     @Override
     public <V, C> List<C> hashSearch(SearchFilter<V>[] searchFilters, Class<C> clazz) {
-        return jedisResource(jedis -> {
+        return getWithJedis(jedis -> {
             final List<C> objects = new ArrayList<>();
             for (String v : jedis.scan("0", new ScanParams().match("*")).getResult()) {
                 try {
@@ -124,9 +123,15 @@ public class Jenin extends JeninParser {
         return getChannelManager(pool.getResource());
     }
 
-    private <R> R jedisResource(Function<Jedis, R> function) {
+    private <R> R getWithJedis(Function<Jedis, R> function) {
         try (Jedis jedis = pool.getResource()) {
             return function.apply(jedis);
+        }
+    }
+
+    private void withJedis(Consumer<Jedis> consumer) {
+        try (Jedis jedis = pool.getResource()) {
+            consumer.accept(jedis);
         }
     }
 }
